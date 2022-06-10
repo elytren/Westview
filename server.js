9a5f7c735aacadd4fb737503d7ab6f5f61ad503d
elytren <elysetrendell@gmail.com>
2022-06-09 18:25:27 -0700

    tailwind installed, might go with bootstrap instead

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const methodOverride = require('method-override');
 const PORT = 3000
 const homeRts = require('./routes/homeRts')
 const reviewRts = require('./routes/reviewsRts')
+const publicPath = path.join(__dirname, 'public');
+
+app.use('/public', express.static(publicPath));
 
 require('./database/connection')
 
@@ -15,16 +18,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 app.use(methodOverride('_method'));
 
+app.use(express.static('./public/stylesheets/style.css'));
+
 app.use('/wv', homeRts)
 app.use('/wv', reviewRts)
-//ROUTES START 
-
-
-
-
-
-//ROUTES END
-
 
 
 app.listen(PORT, ()=>{
