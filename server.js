88310518a6e92858b6fe8d9518c3dfcd274cf6c0
elytren <elysetrendell@gmail.com>
2022-06-19 21:28:00 -0700

    code set for heroku deployment

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ const normalizePort = require('normalize-port')
 const port = normalizePort(process.env.PORT || '3000')
 
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 app.use(methodOverride('_method'));
